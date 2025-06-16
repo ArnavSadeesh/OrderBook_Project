ce68dc17177b3008bce2e5782ec864eff7dbe7d6
Arnav Sadeesh <as0071@princeton.edu>
2025-06-16 18:14:57 -0400

    Cleaned up main.cpp

--- main.cpp
+++ main.cpp
@@ -3,13 +3,20 @@
 
 int main() { 
     Orderbook orderbook; 
-    const OrderId orderId = 1; 
-    orderbook.AddOrder(std::make_shared<Order>
-        (OrderType::GoodTillCancel, orderId, Side::Buy, 100, 10)); 
-    std::cout << orderbook.Size() << std::endl; //1
 
-    orderbook.CancelOrder(orderId); 
-    std::cout << orderbook.Size() << std::endl;  //0
+    //Work with orderbook here
+
+    /*  
+        Ex.
+        const OrderId orderId = 1; 
+
+        orderbook.AddOrder(std::make_shared<Order>
+            (OrderType::GoodTillCancel, orderId, Side::Buy, 100, 10)); 
+        std::cout << "My current orderbook size: " << orderbook.Size() << std::endl; //1
+
+        orderbook.CancelOrder(orderId); 
+        std::cout << "Orderbook size after cancelling my order: " << orderbook.Size() << std::endl; //1
+    */
 
     return 0;
 }
